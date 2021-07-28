08af512ba401f6a1963c9bdf96f51e010359b902
Horu65 <khoruhoru65@gmail.com>
2021-07-28 23:12:40 +0900

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,34 @@
+<template>
+    <h1>About Page</h1>
+    <h2>{{ subtitle }}</h2>
+</template>
+
+<script>
+
+export default ({
+  head() {
+    return {
+        title: 'About Page',
+        meta:[
+            { hid: 'description', name: 'description', content: 'これはAboutページです'}
+        ]
+    }
+  },
+  data() {
+    return {
+        id: this.$route.params.id,
+        users: [
+            {
+                id: 1,
+                name:'john Doe'
+            }
+        ]
+    }
+  },
+  computed:{
+      user(){
+          return this.users.find(user => user.id ==this.id)
+      }
+  }
+})
+</script>
